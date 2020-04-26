353372135efa019019ef6b073aaafc33d8dd002d
muhtadinziqim <muhtadinzm@gmail.com>
2020-04-26 17:40:12 +0700

    faraz

---
+++ faraaz/src/javascript/script.js
@@ -0,0 +1,62 @@
+const btn_kiri = $(".tombol-kiri > img");
+const btn_kanan = $(".tombol-kanan > img");
+const konten = document.getElementById("scroll-customer");
+
+const konten_scroll_with = konten.scrollWidth;
+let konten_scroll_left = konten.scrollLeft;
+
+
+btn_kanan.click(function() {
+    konten_scroll_left = konten.scrollLeft;
+    konten_scroll_left += 230;
+    if (konten_scroll_left >= konten_scroll_with) {
+        $("#scroll-customer").animate({ scrollLeft: konten_scroll_left }, 500, function() {
+            $("#scroll-customer").scrollLeft(konten_scroll_with)
+        })
+        konten_scroll_left = konten_scroll_with - 500;
+    } else {
+        $("#scroll-customer").animate({ scrollLeft: konten_scroll_left }, 500, function() {
+            $("#scroll-customer").scrollLeft(konten_scroll_left)
+        })
+    }
+})
+
+btn_kiri.click(function() {
+    konten_scroll_left = konten.scrollLeft;
+    konten_scroll_left -= 230;
+    if (konten_scroll_left <= 0) {
+        $("#scroll-customer").animate({ scrollLeft: konten_scroll_left }, 500, function() {
+            $("#scroll-customer").scrollLeft(0)
+        })
+        konten_scroll_left = 0;
+    } else {
+        $("#scroll-customer").animate({ scrollLeft: konten_scroll_left }, 500, function() {
+            $("#scroll-customer").scrollLeft(konten_scroll_left)
+        })
+    }
+})
+
+setInterval(() => {
+    geser_kiri()
+}, 10000);
+
+function geser_kiri() {
+    konten_scroll_left = konten.scrollLeft;
+    konten_scroll_left += 230;
+    if (konten_scroll_left >= konten_scroll_with) {
+        $("#scroll-customer").animate({ scrollLeft: konten_scroll_left }, 500, function() {
+            $("#scroll-customer").scrollLeft(0)
+        })
+        konten_scroll_left = 0;
+    } else {
+        $("#scroll-customer").animate({ scrollLeft: konten_scroll_left }, 500, function() {
+            $("#scroll-customer").scrollLeft(konten_scroll_left)
+        })
+    }
+}
+
+$(document).ready(function() {
+    $('.menu-toggle').click(function() {
+        $('nav').toggleClass('active')
+    })
+})

681aa38275b5355381341b607e9be55d3022ab65
Ziqi Maulana <muhtadinzm@gmail.com>
2021-06-27 18:33:29 +0700

    cariresto

--- cariresto/assets/js/active.js
+++ cariresto/assets/js/active.js
@@ -223,7 +223,7 @@
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000,
-        pauseOnHover: false,
+        pauseOnHover: true,
         pauseOnFocus: false,
         responsive: [
             {
